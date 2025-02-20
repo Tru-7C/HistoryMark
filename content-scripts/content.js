b81fb13598720ab151e5419650e53a792ac5d411
Mariko Kawaguchi <mariko@Marikos-MacBook-Air.local>
2025-02-19 22:29:23 -0800

    Changed sidepanel visibility

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -1,3 +1,5 @@
+let markedPage = Boolean(false);
+
 // Set new Favicon to this document
 function setFavicon(svgContent) {
     const base64Svg = btoa(decodeURIComponent(encodeURIComponent(svgContent)));
@@ -20,15 +22,33 @@ document.addEventListener('dblclick', async () => {
         tabInfo: { title: document.title, url: location.href }
     });
     
-    var draw = SVG().size(100, 100);
-    draw.image(response.favicon).size(100, 100);
-    var nested = draw.nested();
-    nested.svg(
-        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">' +
-        '<path fill="#ff0000" stroke="#ff0000" stroke-width="80" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>' +
-        '</svg>'
-    ).size(70, 70).move(30, -10);
+    if (!markedPage)
+    {
+        var draw = SVG().size(100, 100);
+        draw.image(response.favicon).size(100, 100);
+        var nested = draw.nested();
+        nested.svg(
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">' +
+            '<path fill="#26CE5F" stroke="#26CE5F" stroke-width="80" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>' +
+            '</svg>'
+        ).size(70, 70).move(30, -10);
+        
+        const markedFavicon = draw.svg();
+
+        setFavicon(markedFavicon);
+        markedPage = true;
+    }
+    else
+    {
+        const head = document.head;
+        head.querySelectorAll("link[rel~='icon']").forEach((el) => el.remove());
     
-    const markedFavicon = draw.svg();
-    setFavicon(markedFavicon);
+        const newFavicon = document.createElement('link');
+        newFavicon.rel = `icon`;
+        newFavicon.type = `image/png`;
+        newFavicon.href = `data:image/png;base64,${response.favicon.split(',')[1]}`;
+    
+        head.appendChild(newFavicon);
+        markedPage = false;
+    }
 });
--- sidepanel/sidepanel.js
+++ sidepanel/sidepanel.js
@@ -1,27 +1,48 @@
-const historyList = document.getElementById("history-list");
+const tabList = document.getElementById("Tab-list");
+const refreshButton = document.getElementById("refresh-button");
 
+loadTabs();
+
+refreshButton.addEventListener("click", () => {
+    loadTabs();
+});
+
+function loadTabs() {
+    tabList.innerHTML = "";
+
+    // Get all tabs in the current window
+    chrome.tabs.query({ currentWindow: true }, (tabs) => {
+        const tabInfoList = tabs.map(tab => ({
+        id: tab.id,
+        title: tab.title,
+        url: tab.url
+        }));
+
+        // Create tab List
+        tabInfoList.forEach(item => {
+            const listItem = document.createElement("li");
+
+            const faviconUrl = getFaviconURL(item.url);
+            const title = item.title;
+
+            listItem.classList.add('listItemStyle');
+            listItem.innerHTML = `
+                <img src="${faviconUrl}" alt="Favicon">
+                <span class="listTextStyle">${title}</span>`;
+
+            listItem.addEventListener("click", () => {
+                chrome.tabs.update(item.id, { active: true });
+            });
+
+            tabList.appendChild(listItem);
+        });
+    });
+}
+
+// Get Favicon URL
 function getFaviconURL(url) {
     const faviconUrl = new URL(chrome.runtime.getURL('/_favicon/'));
     faviconUrl.searchParams.set('pageUrl', url);
     faviconUrl.searchParams.set('size', '16');
     return faviconUrl.toString();
-  }
-
-chrome.history.search({ text: '', maxResults: 50 }, (results) => {
-    results.forEach(item => {
-        const listItem = document.createElement("li");
-
-        const faviconUrl = getFaviconURL(item.url);
-        const urlTitle = item.title;
-
-        listItem.classList.add('listItemStyle');
-        listItem.innerHTML = `
-            <img src="${faviconUrl}" alt="Favicon">
-            <span class="listTextStyle">${urlTitle}</span>`;
-        listItem.addEventListener('click', () => {
-            chrome.tabs.create({ url: item.url });
-        });
-
-        historyList.appendChild(listItem);
-    });
-});
+}
