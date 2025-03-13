918f5bc2275455461ed62c3fa32c07ded33f3869
Mariko Kawaguchi <mariko@Marikos-MacBook-Air.local>
2025-03-12 22:29:48 -0700

    Added show/hide behavior into Content.js when key <Ctrl+B> is pressed.

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -1,6 +1,28 @@
+var isMarked = false;
+var draw;
+
+CreateBookmarkImage();
+
 document.addEventListener('keydown', event => {
     if (event.ctrlKey && event.code === 'KeyB') {
         event.preventDefault();
-        console.log('<Ctrl + B> pressed.');
+        
+        if (isMarked)
+        {
+            isMarked = false;
+            draw.hide();
+        }
+        else if (!isMarked)
+        {
+            isMarked = true;
+            draw.show();
+        }
     }
 });
+
+function CreateBookmarkImage() {
+    draw = SVG().addTo('body').size(100, 100).attr({ style: 'position: fixed; top: 100; right: 50;' });
+    draw.hide();
+    draw.rect(100, 100).attr({ fill: '#f06' });
+}
+
